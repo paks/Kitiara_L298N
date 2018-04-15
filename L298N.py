d19428eaf3c102a3f9dbd929f6a9e9c08e07c83f
Cesar Mendoza <paks@kitiara.net>
2018-04-15 12:33:05 -0500

    First stab at the code

---
+++ L298N.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+'''
+**********************************************************************
+* Filename    : L298N.py
+* Description : A driver module for L298N
+* Author      : Cavon
+* Brand       : SunFounder
+* E-mail      : service@sunfounder.com
+* Website     : www.sunfounder.com
+* Update      : Cavon    2016-09-23    New release
+**********************************************************************
+'''
+import RPi.GPIO as GPIO
+
+class Motor(object):
+	''' Motor driver class
+		Set direction_channel_a to the GPIO channel which connect to MA, 
+		Set motor_B to the GPIO channel which connect to MB,
+		Both GPIO channel use BCM numbering;
+		Set pwm_channel to the PWM channel which connect to PWMA,
+		Set pwm_B to the PWM channel which connect to PWMB;
+		PWM channel using PCA9685, Set pwm_address to your address, if is not 0x40
+		Set debug to True to print out debug informations.
+	'''
+	_DEBUG = False
+	_DEBUG_INFO = 'DEBUG "L298N.py":'
+
+	def __init__(self, direction_channel_a, direction_channel_b, pwm=None):
+		'''Init a motor on giving dir. channel and PWM channel.'''
+		if self._DEBUG:
+			print self._DEBUG_INFO, "Debug on"
+		self.direction_channel_a = direction_channel_a
+		self.direction_channel_b = direction_channel_b
+		self._pwm = pwm
+		self._speed = 0
+
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+
+		if self._DEBUG:
+			print self._DEBUG_INFO, 'setup motor direction channel at', direction_channel_a
+			print self._DEBUG_INFO, 'setup motor pwm channel as', self._pwm.__name__
+		GPIO.setup(self.direction_channel_a, GPIO.OUT)
+		GPIO.setup(self.direction_channel_b, GPIO.OUT)
+
+	@property
+	def speed(self):
+		return self._speed
+
+	@speed.setter
+	def speed(self, speed):
+		''' Set Speed with giving value '''
+		if speed not in range(0, 101):
+			raise ValueError('speed ranges fron 0 to 100, not "{0}"'.format(speed))
+		if not callable(self._pwm):
+			raise ValueError('pwm is not callable, please set Motor.pwm to a pwm control function with only 1 veriable speed')
+		if self._DEBUG:
+			print self._DEBUG_INFO, 'Set speed to: ', speed
+		self._speed = speed
+		self._pwm(self._speed)
+
+	def forward(self):
+		''' Set the motor direction to forward '''
+		GPIO.output(self.direction_channel_a, GPIO.LOW)
+		GPIO.output(self.direction_channel_b, GPIO.HIGH)
+		self.speed = self._speed
+		if self._DEBUG:
+			print self._DEBUG_INFO, 'Motor moving forward (%s)' % str((GPIO.LOW, GPIO.HIGH))
+
+	def backward(self):
+		''' Set the motor direction to backward '''
+		GPIO.output(self.direction_channel_a, GPIO.HIGH)
+		GPIO.output(self.direction_channel_b, GPIO.LOW)
+		self.speed = self._speed
+		if self._DEBUG:
+			print self._DEBUG_INFO, 'Motor moving backward (%s)' % str((GPIO.HIGH, GPIO.LOW))
+
+	def stop(self):
+		''' Stop the motor by giving a 0 speed '''
+		if self._DEBUG:
+			print self._DEBUG_INFO, 'Motor stop'
+		self.speed = 0
+		GPIO.output(self.direction_channel_a, GPIO.LOW)
+		GPIO.output(self.direction_channel_b, GPIO.LOW)
+
+	@property
+	def debug(self, debug):
+		return self._DEBUG
+
+	@debug.setter
+	def debug(self, debug):
+		''' Set if debug information shows '''
+		if debug in (True, False):
+			self._DEBUG = debug
+		else:
+			raise ValueError('debug must be "True" (Set debug on) or "False" (Set debug off), not "{0}"'.format(debug))
+
+		if self._DEBUG:
+			print self._DEBUG_INFO, "Set debug on"
+		else:
+			print self._DEBUG_INFO, "Set debug off"
+
+	@property
+	def pwm(self):
+		return self._pwm
+
+	@pwm.setter
+	def pwm(self, pwm):
+		if self._DEBUG:
+			print self._DEBUG_INFO, 'pwm set'
+		self._pwm = pwm
+
+
+def test():
+	import time
+
+	print "********************************************"
+	print "*                                          *"
+	print "*                 L298N                    *"
+	print "*                                          *"
+	print "*          Connect MA to BCM17             *"
+	print "*          Connect MB to BCM18             *"
+	print "*         Connect PWMA to BCM27            *"
+	print "*         Connect PWMB to BCM12            *"
+	print "*                                          *"
+	print "********************************************"
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup((5, 6), GPIO.OUT)
+	GPIO.setup((13, 19), GPIO.OUT)
+	a = GPIO.PWM(27, 60)
+	b = GPIO.PWM(22, 60)
+	a.start(0)
+	b.start(0)
+
+	def a_speed(value):
+		a.ChangeDutyCycle(value)
+
+	def b_speed(value):
+		b.ChangeDutyCycle(value)
+
+	motorA = Motor(5,6)
+	motorB = Motor(13,19)
+	motorA.debug = True
+	motorB.debug = True
+	motorA.pwm = a_speed
+	motorB.pwm = b_speed
+
+	delay = 0.05
+
+	motorA.forward()
+	for i in range(0, 101):
+		motorA.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorA.speed = i
+		time.sleep(delay)
+
+	motorA.backward()
+	for i in range(0, 101):
+		motorA.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorA.speed = i
+		time.sleep(delay)
+
+	motorB.forward()
+	for i in range(0, 101):
+		motorB.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorB.speed = i
+		time.sleep(delay)
+
+	motorB.backward()
+	for i in range(0, 101):
+		motorB.speed = i
+		time.sleep(delay)
+	for i in range(100, -1, -1):
+		motorB.speed = i
+		time.sleep(delay)
+
+
+if __name__ == '__main__':
+	test()
---
+++ README.md
@@ -0,0 +1,66 @@
+##  L298N
+L298N
+
+Quick Links:
+
+ * [About Kitiara L298N](#about_this_module)
+ * [Update](#update)
+ * [About ](#about_L298N)
+ * [License](#license)
+ * [Contact us](#contact_us)
+
+<a id="about_this_module"></a>
+### About Kitiara L298N:
+This module is for the PCB board L298N, a 2 Channel Motor Driver.
+
+<a id="update"></a>
+### Update:
+2018-04-15:
+ - New Release
+
+----------------------------------------------
+<a id="about_L298N"></a>
+### About 
+	TODO
+----------------------------------------------
+<a id="license"></a>
+### License
+This is the code for L298N.
+MIT License
+
+Copyright (c) 2018 Cesar Mendoza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ L298N comes with ABSOLUTELY NO WARRANTY; for details run ./show w. This is free software, and you are welcome to redistribute it under certain conditions; run ./show c for details.
+
+, Inc., hereby disclaims all copyright interest in the program ' L298N' (which makes passes at compilers).
+
+Cesar Mendoza, 15 April 2018
+
+Email: paks@kitiara.net
+
+----------------------------------------------
+<a id="contact_us"></a>
+### Contact us:
+website:
+	www.kitiara.net
+
+E-mail:
+	paks@kitiara.net
